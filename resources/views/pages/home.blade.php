f4dd5b0ec3fe323d2d58bed5d928003a09ee6a61
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2024-01-10 22:40:09 -0300

    jogos

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -374,7 +374,7 @@
                 @foreach($chunk as $game)
                     <div class="game-slide">
                         @if(isset($game['image_path']) && is_string($game['image_path']))
-                            <div class="img-game-slide" style="background-image: url('{{ $game['image_path'] }}'); background-position: center; background-size: cover;"></div>
+                        <div class="img-game-slide" style="background-image: url('{{ $game['image_path'] }}'); background-position: center; background-size: 100% 100%;"></div>
                         @else
                             <div class="img-game-slide" style="background-image: url('');"></div>
                         @endif
@@ -445,7 +445,7 @@
                 @foreach($chunk as $game)
                     <div class="game-slide">
                         @if(isset($game['image_path']) && is_string($game['image_path']))
-                            <div class="img-game-slide" style="background-image: url('{{ $game['image_path'] }}');"></div>
+                            <div class="img-game-slide" style="background-image: url('{{ $game['image_path'] }}'); background-position: center; background-size: 100% 100%;"></div>
                         @else
                             <div class="img-game-slide" style="background-image: url('');"></div>
                         @endif
