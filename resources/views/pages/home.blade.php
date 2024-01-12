5d7e56172d727155157dcc67f02867e99e075f40
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2024-01-12 19:32:43 -0300

    revert

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -448,7 +448,7 @@
         slidesPerView: getSlidesPerView(),
         slidesPerColumn: 3,
         spaceBetween: 10,
-        slidesPerGroup: 5,
+        slidesPerGroup: 3,
         navigation: {
             nextEl: '.swiper-button-next-pg',
             prevEl: '.swiper-button-prev-pg',
@@ -459,7 +459,7 @@
         if (window.innerWidth < 768) {
             return 2;
         } else {
-            return 5;
+            return 3;
         }
     }
 
